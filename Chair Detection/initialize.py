b74d1f69efdf6f5adae218cbb22a8d03678b82d8
Shayan Khalili <Shayanlion25@gmail.com>
2019-09-21 23:25:28 -0400

    update python initialization script so it doesn't loop

--- Chair Detection/initialize.py
+++ Chair Detection/initialize.py
@@ -6,13 +6,8 @@ import sys
 camera = PiCamera()
 
 camera.start_preview()
-try:
-    while(True):
-        sleep(3)
-        camera.capture('chairs.jpg')
-        image = {'file': open('chairs.jpg', 'rb')}
-        r = requests.post('https://{0}.ngrok.io/initialize'.format(sys.argv[1]), files = image)
-
-except KeyboardInterrupt:
-    pass
+sleep(5)
+camera.capture('chairs.jpg')
+image = {'file': open('chairs.jpg', 'rb')}
+r = requests.post('https://{0}.ngrok.io/initialize'.format(sys.argv[1]), files = image)
 camera.stop_preview()
